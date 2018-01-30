3a6681b7048de381c345bba945ff578c4d638eed
jpdemory <jpdemory@manastria.fr>
2018-01-30 16:22:00 +0100

    Table de multiplication : affichage simple

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // En utilisant la syntaxe du langage C
+            // Créer une boucle pour afficher la table des 8 (multiplication).
+            // Concaténer : +
+            // Saut de ligne : \n
+            const int multiple = 8;
+            const int valMax = 10;
+
+            string affichage = "";
+            for (int i =0; i <=valMax; i++)
+            {
+                affichage = affichage + i 
+                    + " x " + multiple 
+                    + " = " + multiple * i;
+                 
+                affichage += "\n";
+            }
+            Console.WriteLine(affichage);
+            Console.WriteLine("Press a key");
+            Console.ReadKey();
+        }
+    }
+}
