c7699ee7cc3d633cd9ca14e42b777eb8ce21860e
jpdemory <jpdemory@manastria.fr>
2018-01-30 16:40:37 +0100

    Affichage avec string.Format

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -13,12 +13,17 @@ namespace Demo
             const int multiple = 8;
             const int valMax = 10;
 
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             string affichage = "";
             for (int i =0; i <=valMax; i++)
             {
-                affichage = affichage + i 
-                    + " x " + multiple 
-                    + " = " + multiple * i;
+                affichage += 
+                    string.Format("{0:00} x {1,-4} = {2,10:C}",
+                    i,              // Element : 0
+                    multiple,       // Element : 1
+                    multiple * i    // Element : 2
+                    );
                  
                 affichage += "\n";
             }
